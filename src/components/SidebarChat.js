a582747883f00680dcf8e5e4d316ea392b754445
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 06:03:16 +0300

    Rokas

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -6,6 +6,12 @@ const SidebarChat = () => {
     return (
         <div className="sidebarChat">
             <Avatar />
+
+            <div className="sidebarChat__info">
+                <h3>Channel name</h3>
+                <p>Last message sent...</p>
+                <small>timestamp</small>
+            </div>
         </div>
     );
 };
