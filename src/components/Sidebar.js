3a6f6d94bfc0d389d1ff24a11e67e0746436f85e
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 07:54:15 +0300

    Rokas

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "./styles/Sidebar.css";
 import Avatar from "@material-ui/core/Avatar";
 import SearchIcon from "@material-ui/icons/Search";
@@ -7,29 +7,58 @@ import Modal from "@material-ui/core/Modal";
 import Fade from "@material-ui/core/Fade";
 import {Tooltip, TextField, Button, IconButton} from "@material-ui/core";
 import SidebarChat from "./SidebarChat";
-
+import {useSelector} from "react-redux";
+import {selectUser} from "../features/useSlice";
+import { auth } from "../firebase";
+import db from "../firebase";
 
 const Sidebar = () => {
+
+    const user = useSelector(selectUser);
+    const [chats, setChats] = useState([]);
+
+    useEffect(() => {
+        // listents to the database
+        db.collection('chats').onSnapshot((snapshot) => (
+            // we go into the collection chats, take a snapshot and pull that object with data to the array of objects in chat state
+            setChats(snapshot.docs.map(doc => ({
+                // go per each ones and set to the data to the following keys
+                id: doc.id,
+                data: doc.data(),
+            })))
+        ));
+    }, []);
+
+    const addChat = () => {
+        const chatName = prompt("Please enter a chat name");
+        if(chatName) {
+        db.collection('chats').add({
+            chatName: chatName,
+            })
+        }
+    }
+
     return (
         <div className="sidebar">
 
             <div className="sidebar__header">
-                <Avatar className="sidebar__avatar" />
+                <Avatar onClick={() => auth.signOut()} src={user.photo} className="sidebar__avatar" />
                 <div className="sidebar__input">
                     <SearchIcon />
                     <input type="text" placeholder="Search"/>
                 </div>
 
                 <IconButton className="sidebar__inputButton" variant="outlined">
-                    <RateReviewOutlined />
+                    <RateReviewOutlined  onClick={addChat}/>
                 </IconButton>
 
             </div>
 
             <div className="sidebar__chats">
-                <SidebarChat />
-                <SidebarChat />
-                <SidebarChat />
+                {chats.map(({id, data: { chatName }}) => (
+                    // for each one displaying the component and passing the props from the state which is array of objects
+                    <SidebarChat key={id} id={id} chatName={chatName} />
+                ))}
 
             </div>
         </div>
--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -2,13 +2,13 @@ import React from 'react';
 import "./styles/SidebarChat.css"
 import {Avatar} from "@material-ui/core";
 
-const SidebarChat = () => {
+const SidebarChat = ({id, chatName}) => {
     return (
         <div className="sidebarChat">
             <Avatar />
 
             <div className="sidebarChat__info">
-                <h3>Channel name</h3>
+                <h3>{chatName}</h3>
                 <p>Last message sent...</p>
                 <small>timestamp</small>
             </div>
