5d813513639db91ddc2cb02e2b68a9f3fb293667
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 09:49:52 +0300

    Roaks

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import "./styles/Message.css";
 import {Avatar} from "@material-ui/core";
 import {useSelector} from "react-redux";
 import {selectUser} from "../features/useSlice";
 
-const Message = ({ id, contents: {timestamp, displayName, email, message, photo, uid } }) => {
+const Message = forwardRef(({ id, contents: {timestamp, displayName, email, message, photo, uid } }, ref) => {
     const user = useSelector(selectUser);
 
     return (
-        <div className={`message ${user?.email === email && "message__sender"}`}>
+        <div ref={ref} className={`message ${user?.email === email && "message__sender"}`}>
             <Avatar className="message__photo" src={photo}/>
             <p>{message}</p>
             <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
         </div>
     );
-};
+});
+
 
 export default Message;
