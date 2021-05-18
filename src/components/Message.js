4839208de9df6bde3b86c01c11f311902d1fd20b
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 09:56:01 +0300

    Rokas

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,6 +3,7 @@ import "./styles/Message.css";
 import {Avatar} from "@material-ui/core";
 import {useSelector} from "react-redux";
 import {selectUser} from "../features/useSlice";
+import * as timeago from "timeago.js"
 
 const Message = forwardRef(({ id, contents: {timestamp, displayName, email, message, photo, uid } }, ref) => {
     const user = useSelector(selectUser);
@@ -11,7 +12,7 @@ const Message = forwardRef(({ id, contents: {timestamp, displayName, email, mess
         <div ref={ref} className={`message ${user?.email === email && "message__sender"}`}>
             <Avatar className="message__photo" src={photo}/>
             <p>{message}</p>
-            <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
+            <small>{timeago.format(new Date(timestamp?.toDate()).toLocaleString())}</small>
         </div>
     );
 });
