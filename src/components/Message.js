b72f30a9d16e648517aa17c3fb337cf7b53ede18
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 09:32:17 +0300

    Rokas

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./styles/Message.css";
 import {Avatar} from "@material-ui/core";
+import {useSelector} from "react-redux";
+import {selectUser} from "../features/useSlice";
 
 const Message = ({ id, contents: {timestamp, displayName, email, message, photo, uid } }) => {
+    const user = useSelector(selectUser);
+
     return (
-        <div className="message">
-            <Avatar src={photo}/>
+        <div className={`message ${user?.email === email && "message__sender"}`}>
+            <Avatar className="message__photo" src={photo}/>
             <p>{message}</p>
             <small>{new Date(timestamp?.toDate()).toLocaleString()}</small>
         </div>
