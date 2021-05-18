267c2069f262cb5004bf2ea9f8fa1ff04d62fa2c
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 08:04:07 +0300

    Rokas

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from "../features/useSlice";
+import chatReducer from "../features/chatSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    chat: chatReducer,
   },
 });
