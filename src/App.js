a7a49eab1be536d715610353e9c2f2cbf01cb557
Rokas <rookas.rudzenskas@gmail.com>
2021-05-18 07:12:02 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Imessage from "./components/Imessage"
+import {useSelector} from "react-redux";
+import {selectUser} from "./features/useSlice";
 
 function App() {
-
+    //this pulls the user from redux
+    const user = useSelector(selectUser);
   return (
     <div className="app">
+        {user ? (
         <Imessage />
+        ) : (
+            <Login />
+        )}
     </div>
   );
 }
